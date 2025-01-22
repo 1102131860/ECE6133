0b5672513b41181dd6980b7f7874dce2fb4838ad
Rui Wang <1102131860@qq.com>
2025-01-22 18:10:14 -0500

    Update FM.py and FM_test_file_1.txt, and add a new FM test file

--- code/FM.py
+++ code/FM.py
@@ -6,9 +6,12 @@ class FM_Algorithm:
 
         # graph attributes
         self.degree = 0
+        self.constraints = tuple()
         self.vertices = set()
         self.hyperedges = list()
         self.graph = dict()
+
+        # bucket structure
         self.bucket = dict()
 
         # result records
@@ -54,6 +57,10 @@ class FM_Algorithm:
                             self.graph[v] = set()
                         self.partitions.append(tuple(map(set, parts))) # tuple up the sets
                         continue
+                    
+                    if i == 1:
+                        self.constraints = tuple(map(int, parts)) # lowest_boud & highest_bound
+                        continue
 
                     hyed = set(parts)
                     self.hyperedges.append(hyed)
@@ -78,10 +85,62 @@ class FM_Algorithm:
 
 
     def partionizing(self):
-        # When you select the best move, consider about area constraint
-        pass
-    
+        unlocked_vertices = self.vertices.copy()
+        low_bound, high_bound = self.constraints
+        p1, p2 = self.partitions[0]
 
+        while unlocked_vertices:
+            max_gain_vertice = None
+            max_gain = max((k for k, v in self.bucket.items() if v), default=None)
+
+            # find max_gain_vertice
+            while max_gain is not None:
+                max_gain_vertices = self.bucket[max_gain]
+
+                for v in sorted(max_gain_vertices): # based on alphabetical order
+                    if v in unlocked_vertices: # should in the unlocked_vertices
+                        if v in p1 and low_bound < len(p1) and high_bound > len(p2) or \
+                            v in p2 and low_bound < len(p2) and high_bound > len(p1): # ensure the area constraint
+                            max_gain_vertice = v
+                            break
+                
+                if max_gain_vertice is not None:
+                    break
+                
+                # find the second max gain
+                max_gain = max((k for k, v in self.bucket.items() if v and k < max_gain), default=None)
+
+            # move the selected vertice into opposite partition
+            if max_gain_vertice in p1:
+                p1.remove(max_gain_vertice)
+                p2.add(max_gain_vertice)
+            else:
+                p2.remove(max_gain_vertice)
+                p1.add(max_gain_vertice)
+            
+            # lock the selected vertice
+            unlocked_vertices.remove(max_gain_vertice)
+
+            # remove max_gain_vertice from self.bucket
+            self.bucket[max_gain].remove(max_gain_vertice)
+
+            # update the unclocked neigbours' gain
+            neigbours = self.graph[max_gain_vertice]
+            for v in neigbours:
+                if v in unlocked_vertices: # should in the unlocked_vertices
+                    for gain, vs in self.bucket.items():
+                        if v in vs:
+                            self.bucket[gain].remove(v) # firstly remove neighbour from the old gain
+                    
+                    update_gain = self.gain(v, p1, p2)  # then add new gain
+                    self.bucket[update_gain].add(v)
+
+            # Record the results
+            self.partitions.append((p1.copy(), p2.copy()))
+            self.cutsizes.append(self.cutsize(p1, p2))
+            self.max_gains.append(max_gain)
+            self.max_moves.append(max_gain_vertice)
+        
 
 if __name__ == "__main__":
     # obtain file path from cmd
@@ -94,18 +153,23 @@ if __name__ == "__main__":
 
     FM.initialize()
 
-    # Just for test
+    print("********************************************************************************")
+    print("Initialization:")
     print(f"Degree: {FM.degree}")
     print(f"Vertices: {FM.vertices}")
+    print(f"Constraints: {FM.constraints}")
     print(f"Hyperedges: {FM.hyperedges}")
     print(f"Graph: {FM.graph}")
     print(f"Bucket: {FM.bucket}")
     print(f"Partitions: {FM.partitions}")
     print(f"Cutsizes: {FM.cutsizes}")    
+
+    FM.partionizing()
     
-    max_gain = max((k for k, v in FM.bucket.items() if v), default=None)
-    max_gain_vertices = FM.bucket[max_gain]
-    max_gain_vertice = min(max_gain_vertices)  # based on the alphabatical order
-    print(f"max_gain of initial partiton: {max_gain}")
-    print(f"max_gain_vertices of initial partition: {max_gain_vertices}") 
-    print(f"Based on alphabetical order, then best max_gain_vertices is {max_gain_vertice}")
+    print(f"\n*******************************************************************************")
+    print("Partitioning:")
+    # show the partitionized results
+    for i in range(len(FM.partitions)):
+        print(f"Iteration {i}. partitions: {FM.partitions[i]}, cutsizes: {FM.cutsizes[i]}")
+    for i in range(len(FM.max_gains)):
+        print(f"Move {i+1}: {FM.max_moves[i]}, gain: {FM.max_gains[i]}")
